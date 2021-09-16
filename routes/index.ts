3de2da3a4eaa0739cd109d4d88daa4b9ebc3524d
Carlos Rafael Gimenes das Neves <carlosrafael.prog@gmail.com>
2021-09-16 17:09:47 -0300

    Primeiro commit

---
+++ routes/index.ts
@@ -0,0 +1,58 @@
+﻿import app = require("teem");
+import appsettings = require("../appsettings");
+import Usuario = require("../models/usuario");
+
+class IndexRoute {
+	public static async index(req: app.Request, res: app.Response) {
+		let u = await Usuario.cookie(req);
+		if (!u)
+			res.redirect(app.root + "/login");
+		else
+			res.render("index/index", { titulo: "Dashboard", usuario: u });
+	}
+
+	@app.http.all()
+	public static async login(req: app.Request, res: app.Response) {
+		let u = await Usuario.cookie(req);
+		if (!u) {
+			const token = req.query["token"] as string;
+
+			if (token) {
+				const [mensagem, u] = await Usuario.efetuarLogin(token, res);
+				if (mensagem)
+					res.render("index/login", { layout: "layout-externo", mensagem: mensagem, ssoRedir: appsettings.ssoRedir });
+				else
+					res.redirect(app.root + "/");
+			} else {
+				res.render("index/login", { layout: "layout-externo", mensagem: null, ssoRedir: appsettings.ssoRedir });
+			}
+		} else {
+			res.redirect(app.root + "/");
+		}
+	}
+
+	public static async acesso(req: app.Request, res: app.Response) {
+		let u = await Usuario.cookie(req);
+		if (!u)
+			res.redirect(app.root + "/login");
+		else
+			res.render("index/acesso", { titulo: "Sem Permissão", usuario: u });
+	}
+
+	public static async perfil(req: app.Request, res: app.Response) {
+		let u = await Usuario.cookie(req);
+		if (!u)
+			res.redirect(app.root + "/");
+		else
+			res.render("index/perfil", { titulo: "Meu Perfil", usuario: u });
+	}
+
+	public static async logout(req: app.Request, res: app.Response) {
+		let u = await Usuario.cookie(req);
+		if (u)
+			await Usuario.efetuarLogout(u, res);
+		res.redirect(app.root + "/");
+	}
+}
+
+export = IndexRoute;
