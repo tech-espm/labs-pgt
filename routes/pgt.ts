f86e2189981991f775bc60559e936635cb600bd2
Leonardo Nunes Negrão <58406267+LeonardoNunes-N@users.noreply.github.com>
2021-12-08 11:28:51 -0300

    Inserindo prototipo de mais variaveis do formulario

--- routes/pgt.ts
+++ routes/pgt.ts
@@ -18,6 +18,21 @@ class PGTRoute {
 				fases: fases.lista,
 				usuarios: await Usuario.listarCombo()
 			});
+	} 
+
+	public static async criarprototipo(req: app.Request, res: app.Response) {
+		let u = await Usuario.cookie(req);
+		if (!u || !u.admin)
+			res.redirect(app.root + "/acesso");
+		else
+			res.render("pgt/editar1prototipo", {
+				titulo: "Criar PGT 1",
+				usuario: u,
+				item: null,
+				tipos: tipos.lista, 
+				fases: fases.lista,
+				usuarios: await Usuario.listarCombo()
+			});
 	}
 
 	public static async editar1(req: app.Request, res: app.Response) {
@@ -39,6 +54,27 @@ class PGTRoute {
 					usuarios: await Usuario.listarCombo()
 				});
 		}
+	} 
+
+	public static async editar1prototipo(req: app.Request, res: app.Response) {
+		let u = await Usuario.cookie(req);
+		if (!u || !u.admin) {
+			res.redirect(app.root + "/acesso");
+		} else {
+			let id = parseInt(req.query["id"] as string);
+			let item: PGT = null;
+			if (isNaN(id) || !(item = await PGT.obter1(id)))
+				res.render("index/nao-encontrado", { usuario: u });
+			else
+				res.render("pgt/editar1prototipo", {
+					titulo: "Editar PGT 1",
+					usuario: u,
+					item: item,
+					tipos: tipos.lista,
+					fases: fases.lista,
+					usuarios: await Usuario.listarCombo()
+				});
+		}
 	}
 
 	public static async editar2(req: app.Request, res: app.Response) {
@@ -52,6 +88,27 @@ class PGTRoute {
 				res.render("index/nao-encontrado", { usuario: u });
 			else
 				res.render("pgt/editar2", {
+					titulo: "Editar PGT 2",
+					usuario: u,
+					item: item,
+					tipos: tipos.lista, 
+					fases: fases.lista,
+					usuarios: await Usuario.listarCombo()
+				});
+		}
+	} 
+
+	public static async editar2prototipo(req: app.Request, res: app.Response) {
+		let u = await Usuario.cookie(req);
+		if (!u || !u.admin) {
+			res.redirect(app.root + "/acesso");
+		} else {
+			let id = parseInt(req.query["id"] as string);
+			let item: PGT = null;
+			if (isNaN(id) || !(item = await PGT.obter2(id)))
+				res.render("index/nao-encontrado", { usuario: u });
+			else
+				res.render("pgt/editar2prototipo", {
 					titulo: "Editar PGT 2",
 					usuario: u,
 					item: item,
