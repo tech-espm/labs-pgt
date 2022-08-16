c946289cab4b4d31119e5192ab6906fc58d4e734
Carlos Rafael Gimenes das Neves <carlosrafael.prog@gmail.com>
2022-08-16 10:02:31 -0300

    Atualizando a lib

--- public/js/main.js
+++ public/js/main.js
@@ -107,9 +107,9 @@ window.Cookies = {
 	}
 };
 window.encode = (function () {
-	var amp = /\&/g, lt = /</g, gt = />/g, quot = /\"/g, apos = /\'/g;
+	var amp = /\&/g, lt = /</g, gt = />/g, quot = /\"/g, apos = /\'/g, grave = /\`/g;
 	window.encodeValue = function (x) {
-		return (x ? x.replace(amp, "&amp;").replace(lt, "&lt;").replace(gt, "&gt;").replace(quot, "&quot;").replace(apos, "&apos;") : "");
+		return (x ? x.replace(amp, "&amp;").replace(lt, "&lt;").replace(gt, "&gt;").replace(quot, "&#34;").replace(apos, "&#39;").replace(grave, "&#96;") : "");
 	};
 	return function (x) {
 		return (x ? x.replace(amp, "&amp;").replace(lt, "&lt;").replace(gt, "&gt;") : "");
